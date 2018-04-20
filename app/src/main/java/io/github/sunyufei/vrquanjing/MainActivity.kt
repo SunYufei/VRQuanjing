fb7bff4eb2f8a78a04adcb7e907ceaacad562c57
SunYufei <sunyufei963@gmail.com>
2018-04-20 11:08:43 +0800

    Update 18/4/20

--- app/src/main/java/io/github/sunyufei/vrquanjing/MainActivity.kt
+++ app/src/main/java/io/github/sunyufei/vrquanjing/MainActivity.kt
@@ -1,15 +1,26 @@
 package io.github.sunyufei.vrquanjing
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.util.DisplayMetrics
+import android.view.WindowManager
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ImageView
 import android.widget.Toast
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.qrcode.QRCodeWriter
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val URL: String = "file:///android_asset/index.html"
+        private const val BASE_URL: String = "https://sunovo.gitee.io/quanjing/"
     }
 
     private lateinit var webView: WebView
@@ -26,6 +37,7 @@ class MainActivity : AppCompatActivity() {
         val settings = webView.settings
         settings.allowFileAccess = true
         settings.allowUniversalAccessFromFileURLs = true
+        settings.cacheMode = WebSettings.LOAD_DEFAULT
         settings.defaultTextEncodingName = "UTF-8"
         settings.domStorageEnabled = true
         settings.javaScriptEnabled = true
@@ -40,6 +52,26 @@ class MainActivity : AppCompatActivity() {
                 webView.loadUrl(url)
                 return true
             }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                when (url == URL) {
+                    true -> this@MainActivity.window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+                    false -> this@MainActivity.window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+                }
+                super.onPageFinished(view, url)
+            }
+        }
+
+        webView.setOnLongClickListener {
+            when (webView.url == URL) {
+                false -> {
+                    val localUrl: String = webView.url
+                    val index = localUrl.indexOf("file:///android_asset/") + "file:///android_asset/".length
+                    val newUrl = BASE_URL + localUrl.substring(index)
+                    showQRCode(newUrl)
+                }
+            }
+            true
         }
     }
 
@@ -59,5 +91,40 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+
+    private fun showQRCode(url: String?) {
+        val imageView = ImageView(this@MainActivity)
+        val qrCodeWriter = QRCodeWriter()
+
+        val windowManager = this@MainActivity.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        val size = when (displayMetrics.widthPixels > displayMetrics.heightPixels) {
+            true -> (displayMetrics.heightPixels * 0.75).toInt()
+            false -> (displayMetrics.widthPixels * 0.75).toInt()
+        }
+
+        val bitMatrix = qrCodeWriter.encode(url, BarcodeFormat.QR_CODE, size, size)
+
+        val pixels = IntArray(size * size)
+        for (i in 0 until size)
+            for (j in 0 until size) when (bitMatrix.get(i, j)) {
+                true -> pixels[i * size + j] = Color.BLACK
+                false -> pixels[i * size + j] = Color.WHITE
+            }
+
+        val bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888)
+        bitmap.setPixels(pixels, 0, size, 0, 0, size, size)
+
+        imageView.setImageBitmap(bitmap)
+
+        val builder = AlertDialog.Builder(this@MainActivity)
+        builder.setTitle("分享")
+        builder.setView(imageView)
+        builder.setPositiveButton("关闭", null)
+        builder.show()
     }
 }
